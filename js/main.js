ef814a720887c954be2f116eab0ee3bbdcc7b0a4
Joe Ziemba <jziemba90@gmail.com>
2015-09-14 17:04:50 -0400

    Navigation and home page added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 
+$(document).ready(function() {
+	
+	$("#menu-btn").click(function() {
+		$("#main-nav").toggleClass('.hidden');
+	})
+
+	$(".hover-panel").hover(
+			function(event) {
+				$(this).stop().fadeTo('400', 1)
+			},
+			function(event) {
+				$(this).fadeTo('400', 0)
+			}
+	);
+
+});
